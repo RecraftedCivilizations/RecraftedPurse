f5c3fbddad134d296df2cae6430cf1189ca99016
darkvanityoflight <darkvanity.oflight@gmail.com>
2022-08-06 13:18:48 +0200

    fix: exchange toString with [0] and remove getOfflinePlayer becuase we don t use it

--- src/main/kotlin/com/github/recraftedcivilizations/recraftedpurse/commands/Wealth.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/recraftedpurse/commands/Wealth.kt
@@ -13,14 +13,13 @@ class Wealth(private val accountManager: AccountManager, private val currencySym
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 
-        val name = args.toString()
+        val name = args[0]
         val uuid = Bukkit.getPlayer(name)?.uniqueId
 
         if(uuid == null){
             sender.sendMessage("${Color.RED}No such player!")
             return true
         }
-        val offlinePlayer = Bukkit.getOfflinePlayer(uuid)
         val wealth = accountManager.accountValue(uuid)
 
         sender.sendMessage("${Color.GREEN}The player $name has a total wealth of $wealth$currencySymbol")
