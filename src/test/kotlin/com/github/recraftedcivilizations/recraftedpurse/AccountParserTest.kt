8c994403c367f7883544dcd0bfdd0e9e52858a37
darkvanityoflight <darkvanity.oflight@gmail.com>
2022-08-25 14:05:45 +0200

    feat: Test saving accounts with the account parser

--- src/test/kotlin/com/github/recraftedcivilizations/recraftedpurse/AccountParserTest.kt
+++ src/test/kotlin/com/github/recraftedcivilizations/recraftedpurse/AccountParserTest.kt
@@ -1,16 +1,57 @@
 package com.github.recraftedcivilizations.recraftedpurse
 
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import java.io.File
+import java.util.UUID
 
 internal class AccountParserTest {
+    private lateinit var dataFile: File
+
+    @BeforeEach
+    fun file(){
+        dataFile = File("./data.yml/")
+    }
+
+    @AfterEach
+    fun cleanUpDataFile(){
+
+        dataFile.delete()
+
+    }
 
     @Test
     fun loadAccount() {
+
     }
 
     @Test
     fun saveAccount() {
+        val uuid1 = UUID.randomUUID()
+        val account1 = Account(uuid1, 10, 10)
+        val uuid2 = UUID.randomUUID()
+        val account2 = Account(uuid2, 20, 15)
+
+        val accountParser = AccountParser("./")
+        accountParser.saveAccount(account1)
+        accountParser.saveAccount(account2)
+
+        assertEquals("accounts:\n" +
+                "  $uuid1:\n" +
+                "    purseBalance: 10\n" +
+                "    bankBalance: 10\n" +
+                "  $uuid2:\n" +
+                "    purseBalance: 15\n" +
+                "    bankBalance: 20\n", readFile(dataFile))
+
+    }
+
+    private fun readFile(file: File): String {
+        val inputStream = file.inputStream()
+        return inputStream.bufferedReader().use { it.readText() }
+
     }
 }
