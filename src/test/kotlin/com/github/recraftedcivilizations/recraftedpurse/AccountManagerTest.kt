baee4b5e9c483a03187a541a6fac9326b829b6fc
darkvanityoflight <darkvanity.oflight@gmail.com>
2022-09-08 17:06:26 +0200

    feat: Test hasInPurse

--- src/test/kotlin/com/github/recraftedcivilizations/recraftedpurse/AccountManagerTest.kt
+++ src/test/kotlin/com/github/recraftedcivilizations/recraftedpurse/AccountManagerTest.kt
@@ -78,6 +78,33 @@ internal class AccountManagerTest {
 
     @Test
     fun hasInPurse() {
+
+        val uuid1 = UUID.randomUUID()
+        val uuid2 = UUID.randomUUID()
+        val uuid3 = UUID.randomUUID()
+
+        val account1 = mockk<Account>()
+        val account2 = mockk<Account>()
+        val account3 = mockk<Account>()
+
+        every { accountParser.loadAccount(uuid1) } returns account1
+        every { accountParser.loadAccount(uuid2) } returns account2
+        every { accountParser.loadAccount(uuid3) } returns account3
+
+        every { account1.purseBalance } returns 10
+        every { account2.purseBalance } returns 5
+        every { account3.purseBalance } returns 15
+
+        val accountManager = AccountManager(true, 0.0, accountParser)
+
+        assert(accountManager.hasInPurse(uuid1, 10))
+        verify { account1.purseBalance }
+        assert(!accountManager.hasInPurse(uuid2, 10))
+        verify { account2.purseBalance }
+        assert(accountManager.hasInPurse(uuid3, 10))
+        verify { account3.purseBalance }
+
+
     }
 
     @Test
