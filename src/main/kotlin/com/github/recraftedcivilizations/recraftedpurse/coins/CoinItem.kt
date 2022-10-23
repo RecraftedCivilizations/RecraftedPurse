6a9b03a8c56685da59bd522ad12794fc72c8ae40
darkvanityoflight <darkvanity.oflight@gmail.com>
2022-10-23 11:23:54 +0200

    fix: Add an actual check if it is a coin of this type

--- src/main/kotlin/com/github/recraftedcivilizations/recraftedpurse/coins/CoinItem.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/recraftedpurse/coins/CoinItem.kt
@@ -1,14 +1,22 @@
 package com.github.recraftedcivilizations.recraftedpurse.coins
 
+import com.github.recraftedcivilizations.recraftedpurse.gui.elements.getLore
+import com.github.recraftedcivilizations.recraftedpurse.gui.elements.getName
 import com.github.recraftedcivilizations.recraftedpurse.gui.elements.setLore
 import com.github.recraftedcivilizations.recraftedpurse.gui.elements.setName
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
+/**
+ * @param name The name of the coin item
+ * @param lore The lore this coin should have
+ * @param material The material this coin should consist off
+ * @param value The value of a single coin
+ */
 class CoinItem(val name: String, val lore: String, val material: Material, val value: Int){
 
     fun isCoin(itemStack: ItemStack) : Boolean{
-        return true
+        return itemStack.type == material && itemStack.getLore() == lore && itemStack.getName() == name
     }
 
     fun makeCoin(amount: Int): ItemStack{
